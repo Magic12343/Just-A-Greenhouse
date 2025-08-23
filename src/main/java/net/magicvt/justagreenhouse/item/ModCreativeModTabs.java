c8d1d3b63771175257bd1fa78e1c9af053e011d6
MagicVT <maxgamerspro@gmail.com>
2025-08-23 18:06:07 -0300

    Optimization + Tab Order
    I cleaned up a bit the Creative Tab.

--- src/main/java/net/magicvt/justagreenhouse/item/ModCreativeModTabs.java
+++ src/main/java/net/magicvt/justagreenhouse/item/ModCreativeModTabs.java
@@ -2,11 +2,13 @@ package net.magicvt.justagreenhouse.item;
 
 import net.magicvt.justagreenhouse.JustAGreenhouse;
 import net.magicvt.justagreenhouse.block.ModBlocks;
+import net.magicvt.justagreenhouse.compat.registry.*;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -31,12 +33,107 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.SEED_MAKER.get());
                         pOutput.accept(ModItems.HOMEWORK.get());
 
-                        pOutput.accept(ModBlocks.WHEAT_SEED_BAG.get());
+                        // SEEDBAGS
+                        //SPRING
+
+                        if (ModList.get().isLoaded("collectorsreap")) {
+                            CRCompatBlocks.LIME_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+
+                        //SPRING AUTUMN
+                        if (ModList.get().isLoaded("veggiesdelight")) {
+                            VDCompatBlocks.BROCCOLI_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+
+                        //SPRING WINTER
+                        if (ModList.get().isLoaded("berry_good")) {
+                            BGCompatBlocks.SWEET_BERRIES_PIPS_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+
+                        // SPRING SUMMER AUTUMN
+                        if (ModList.get().isLoaded("veggiesdelight")) {
+                            VDCompatBlocks.GARLIC_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+
+                        //SPRING AUTUMN WINTER
+                        if (ModList.get().isLoaded("veggiesdelight")) {
+                            VDCompatBlocks.TURNIP_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+
+                        //SUMMER
                         pOutput.accept(ModBlocks.MELON_SEED_BAG.get());
                         pOutput.accept(ModBlocks.TORCHFLOWER_SEED_BAG.get());
                         pOutput.accept(ModBlocks.PITCHER_POD_BAG.get());
+                        if (ModList.get().isLoaded("farmersdelight")) {
+                            FDCompatBlocks.TOMATO_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("veggiesdelight")) {
+                            VDCompatBlocks.ZUCCHINI_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("delightful")) {
+                            DFCompatBlocks.SALMONBERRY_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("delightful")) {
+                            DFCompatBlocks.CANTALOUPE_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+
+                        //SUMMER AUTUMN
+                        pOutput.accept(ModBlocks.WHEAT_SEED_BAG.get());
+                        if (ModList.get().isLoaded("veggiesdelight")) {
+                            VDCompatBlocks.BELLPEPPER_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("collectorsreap")) {
+                            CRCompatBlocks.POMEGRANATE_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+
+                        //AUTUMN
                         pOutput.accept(ModBlocks.BEETROOT_SEED_BAG.get());
                         pOutput.accept(ModBlocks.PUMPKIN_SEED_BAG.get());
+
+                        //AUTUMN WINTER
+                        if (ModList.get().isLoaded("farmersdelight")) {
+                            FDCompatBlocks.CABBAGE_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("veggiesdelight")) {
+                            VDCompatBlocks.CAULIFLOWER_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+
+                        //WINTER
+
+                        //YEAR AROUND
+                        if (ModList.get().isLoaded("berry_good")) {
+                            BGCompatBlocks.GLOW_BERRIES_PIPS_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("atmospheric")) {
+                            ATCompatBlocks.ALOE_KERNELS_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("atmospheric")) {
+                            ATCompatBlocks.DRAGON_ROOTS_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("atmospheric")) {
+                            ATCompatBlocks.PASSIONFRUIT_VINE_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("neapolitan")) {
+                            NTCompatBlocks.STRAWBERRY_PIPS_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("neapolitan")) {
+                            NTCompatBlocks.MINT_SPROUTS_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("ramadandelight")) {
+                            RDCompatBlocks.PARSLEY_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("hearthandharvest")) {
+                            HHCompatBlocks.COTTON_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("hearthandharvest")) {
+                            HHCompatBlocks.BLUEBERRY_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("hearthandharvest")) {
+                            HHCompatBlocks.RASPBERRY_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
+                        if (ModList.get().isLoaded("farmersrespite")) {
+                            FRCompatBlocks.TEA_SEED_BAG_ITEM.ifPresent(pOutput::accept);
+                        }
 
                     })
                     .build());
